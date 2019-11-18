b2cf9dacb64627f6567d2a5f02d858525f018514
Kerri-Ann <kaybee1jam@yahoo.com>
2019-11-18 13:33:30 -0500

    initial redux set up

--- refugee-stories/src/index.js
+++ refugee-stories/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './reducer';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+// Redux
+import { createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
