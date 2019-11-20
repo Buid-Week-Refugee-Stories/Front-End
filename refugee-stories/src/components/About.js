8a06125de25f9058a8fdf778aa05acf46a9cb3ea
Heather Nuffer <zzzuzz@gmail.com>
2019-11-19 22:08:42 -0700

    Fixed About component styling

--- refugee-stories/src/components/About.js
+++ refugee-stories/src/components/About.js
@@ -3,9 +3,9 @@ import handsTogether from '../images/handsTogether.jpg';
 
 function About() {
     return (
-      <div style={{textAlign: 'center'}}> 
-        <h1>About Us</h1>
-        <p>Refugee Stories Group</p>
+      <div> 
+        <h1 className='mainH1'>About Us</h1>
+        <p className='homeText'>Refugee Stories Group</p>
         <div className='imgContainer'>
           <img src={handsTogether} alt='hands together' />
         </div>
